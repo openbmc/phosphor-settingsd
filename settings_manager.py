b025ef5dcd16a32f7d3fe7d367b0362bdb38b30f
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2016-09-20 17:48:19 +0530

    Validate user inputs in settings
    Test msg
    
    Change-Id: I0731ce6e00ab3cb4e11deb98c03fda8d5adad913
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- settings_manager.py
+++ settings_manager.py
@@ -12,6 +12,7 @@ from obmc.dbuslib.bindings import DbusProperties, get_dbus
 settings_file_path = os.path.join(sys.prefix, 'share/obmc-phosphor-settings')
 sys.path.insert(1, settings_file_path)
 import settings_file as s
+import re
 
 DBUS_NAME = 'org.openbmc.settings.Host'
 OBJ_NAME = '/org/openbmc/settings/host0'
@@ -20,9 +21,9 @@ CONTROL_INTF = 'org.openbmc.Settings'
 
 class HostSettingsObject(DbusProperties):
     def __init__(self, bus, name, settings, path):
-        DbusProperties.__init__(self)
+        super(HostSettingsObject, self).__init__(validator=self.input_validator, path=name)
         dbus.service.Object.__init__(self, bus, name)
-
+        self.bus = bus
         self.path = path
         if not os.path.exists(path):
             os.mkdir(path)
@@ -58,11 +59,11 @@ class HostSettingsObject(DbusProperties):
         if bmcv:
             value = bmcv
         if type == "i":
-            self.Set(DBUS_NAME, name, value)
+            self.Set(DBUS_NAME, name, int(value))
         elif type == "s":
             self.Set(DBUS_NAME, name, str(value))
         elif type == "b":
-            self.Set(DBUS_NAME, name, value)
+            self.Set(DBUS_NAME, name, int(value))
 
     # Save the settings to the BMC. This will write the settings value in
     # individual files named by the property name to the BMC.
@@ -84,6 +85,51 @@ class HostSettingsObject(DbusProperties):
     @dbus.service.signal(DBUS_NAME, signature='s')
     def SettingsUpdated(self, sname):
         pass
+
+    # Validates the input matching a regular expression
+    def validate_regex(self, regex, value):
+        if not re.compile(regex).search(value):
+            raise ValueError, "Invalid input"
+
+    # Validates the input matching a range
+    def validate_range(self, min, max, value):
+        if value not in range(min, max):
+            raise ValueEror, "Invalid input"
+
+    # Validates the input matching a pre defined list
+    def validate_list(self, list, value):
+        if value not in list:
+            raise ValueEror, "Invalid input"
+
+    # Offloading the validation to plugins for complex inputs
+    def validate_plugin(self, busname, objpath, intf, method, value):
+        plugin = self.bus.get_object(busname, objpath)
+	plugin_method = plugin.get_dbus_method(method, intf)
+	plugin_method()
+
+    def input_validator(self, iface, property, value):
+	settings = s.SETTINGS
+        # Validate to see if the changes are in order
+        # TODO when this changes to multiple levels, need to make a change here.
+        # Idea is to use self.path and enter into needed section
+        for i in settings['host'].iterkeys():
+            shk = settings['host'][i]
+	    if shk['name'] == property:
+                if shk['validation'] == 'list':
+                    self.validate_list(shk['allowed'], value)
+
+                elif shk['validation'] == 'range':
+		    self.validate_range(shk['min'], shk['max']+1, value)
+
+		elif shk['validation'] == 'regex':
+		    self.validate_regex(shk['regex'], value)
+
+		elif shk['validation'] == 'plugin':
+		    self.validate_plugin(shk['service'], shk['obj'], \
+                            shk['intf'], shk['method'], value)
+
+                # We have processed the input.
+                break
 
 if __name__ == '__main__':
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
