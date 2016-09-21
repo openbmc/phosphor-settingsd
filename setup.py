087027dad47fcaf18cf7db3716717097c0f3e4f2
Brad Bishop <bradleyb@us.ibm.com>
2016-09-21 14:50:36 -0400

    Generate settings file during build
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>
    Change-Id: If8831a33757a71bf4c544d7b1a57433361bc4fc8

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from distutils.core import setup
+from distutils.command.build import build
+import yaml
+import pprint
+
+class CustomBuild(build):
+    def run(self):
+        build.run(self)
+
+        SETTINGS_FILE = 'settings.yaml'
+        OUTPUT_FILE = 'settings_file.py'
+
+        with open(SETTINGS_FILE) as s:
+            data = yaml.safe_load(s)
+
+        with open(OUTPUT_FILE, 'w') as f:
+            f.write('SETTINGS=\\\n')
+            pprint.pprint(data, stream=f)
 
 setup(
     name='obmc-phosphor-settings',
     version='1.0',
     scripts=['settings_manager.py'],
-    data_files=[('obmc-phosphor-settings', ['settings_file.py'])])
+    data_files=[('obmc-phosphor-settings', ['settings_file.py'])],
+    cmdclass={'build': CustomBuild})
