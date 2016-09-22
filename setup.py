750b73054a5c58a473c1fe7f331461afc5e48558
Brad Bishop <bradleyb@us.ibm.com>
2016-09-21 20:19:41 -0400

    setup: minor formatting changes
    Change-Id: I899f15dcb98524cc96d22afae7c9c07bdc0751b5
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
-setup(name='obmc-phosphor-settings',
-      version='1.0',
-      scripts=['settings_manager.py'],
-      data_files=[('obmc-phosphor-settings', ['settings_file.py'])],
-      )
+setup(
+    name='obmc-phosphor-settings',
+    version='1.0',
+    scripts=['settings_manager.py'],
+    data_files=[('obmc-phosphor-settings', ['settings_file.py'])])
