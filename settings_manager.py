22c7a1afa5d92009339a238a1d0cf0ca0e353aef
Sergey Solomin <sergey.solomin@us.ibm.com>
2016-10-03 09:44:35 -0500

    phosphor-settingsd - Support YAML categories and sub-categories
    Currently phosphor-settingsd only supports a single category of 'host0',
    but the YAML format supports categories and sub-categories.
    This enhancemend supports categories and sub-categories and
    automatically creates corresponding dbus objects to represent.
    
    Resolves openbmc/openbmc#551
    
    Change-Id: I42bf246881ca08e8429771666415321fa997f0dc
    Signed-off-by: Sergey Solomin <sergey.solomin@us.ibm.com>

--- settings_manager.py
+++ settings_manager.py
@@ -10,25 +10,55 @@ import sys
 from obmc.dbuslib.bindings import DbusProperties, get_dbus
 from IPy import IP
 
-settings_file_path = os.path.join(
-    sys.prefix,
-    'share',
-    'phosphor-settings')
+settings_file_path = os.path.join(sys.prefix, 'share/obmc-phosphor-settings')
 sys.path.insert(1, settings_file_path)
 import settings_file as s
 import re
 
 DBUS_NAME = 'org.openbmc.settings.Host'
-OBJ_NAME = '/org/openbmc/settings/host0'
 CONTROL_INTF = 'org.openbmc.Settings'
 
+def walk_nest(d, keys =()):
+    if type(d) == type({}):
+        for k in d:
+            for rv in walk_nest(d[k], keys + (k, )):
+                yield rv
+    else:
+        yield keys, d
+
+#Parses dictionary file and returns all objects and main properties
+#(name, type, default) for each attribute in the following format:
+#[DBUS_NAME, OBJ_NAME, name, type, default]
+def create_object(settings):
+    allobjects = []
+    lastattr = ''
+    for compound_key, val in walk_nest(settings):
+        attribute = compound_key[len(compound_key) - 2]
+        if attribute != lastattr and lastattr != '':
+            allobjects.append([dbus_name, object, name, type, default])
+            name = ''
+            type = ''
+            default = ''
+        lastattr = attribute
+        dbus_name = compound_key[0]
+        object = compound_key[0].lower()
+        object = object.replace(".","/")
+        object = "/" + object + "0"
+        for i in compound_key[1:len(compound_key)-2]:
+            object = object + "/" + i
+        if compound_key[len(compound_key) - 1] == 'name':
+            name = val;
+        elif compound_key[len(compound_key) - 1] == 'type':
+            type = val;
+        elif compound_key[len(compound_key) - 1] == 'default':
+            default = val;
+    allobjects.append([dbus_name, object, name, type, default])
+    return allobjects
 
 class HostSettingsObject(DbusProperties):
-    def __init__(self, bus, name, settings, path):
-        super(HostSettingsObject, self).__init__(
-            conn=bus,
-            object_path=name,
-            validator=self.input_validator)
+    def __init__(self, bus, name, allobjects, path):
+        super(HostSettingsObject, self).__init__(conn=bus, object_path=name,
+                                                validator=self.input_validator)
         self.bus = bus
         self.path = path
         # Needed to ignore the validation on default networkconfig values as
@@ -43,14 +73,16 @@ class HostSettingsObject(DbusProperties):
             self.settings_signal_handler,
             dbus_interface="org.freedesktop.DBus.Properties",
             signal_name="PropertiesChanged",
-            path="/org/openbmc/settings/host0")
+            path=name)
 
         # Create the dbus properties
-        for i in settings[DBUS_NAME].iterkeys():
-            shk = settings[DBUS_NAME][i]
-            self.set_settings_property(shk['name'],
-                                       shk['type'],
-                                       shk['default'])
+        for prty in allobjects:
+            if prty[1] != name:
+                continue
+            self.set_settings_property(prty[0],
+                                       prty[2],
+                                       prty[3],
+                                       prty[4])
         # Done with consuming factory settings.
         self.adminmode = False
 
@@ -66,16 +98,16 @@ class HostSettingsObject(DbusProperties):
     # This will be either a value previously set,
     # or the default file value if the BMC value
     # does not exist.
-    def set_settings_property(self, name, type, value):
+    def set_settings_property(self, dbus_name, name, type, value):
         bmcv = self.get_bmc_value(name)
         if bmcv:
             value = bmcv
         if type == "i":
-            self.Set(DBUS_NAME, name, int(value))
+            self.Set(dbus_name, name, int(value))
         elif type == "s":
-            self.Set(DBUS_NAME, name, str(value))
+            self.Set(dbus_name, name, str(value))
         elif type == "b":
-            self.Set(DBUS_NAME, name, bool(value))
+            self.Set(dbus_name, name, bool(value))
 
     # Save the settings to the BMC. This will write the settings value in
     # individual files named by the property name to the BMC.
@@ -100,15 +132,15 @@ class HostSettingsObject(DbusProperties):
 
     def validate_regex(self, regex, value):
         if not re.compile(regex).search(value):
-            raise ValueError("Invalid input. Data does not satisfy regex")
+            raise ValueError, "Invalid input. Data does not satisfy regex"
 
     def validate_range(self, min, max, value):
         if value not in range(min, max):
-            raise ValueError("Invalid input. Data not in allowed range")
+            raise ValueError, "Invalid input. Data not in allowed range"
 
     def validate_list_ignore_case(self, lst, value):
         if value.lower() not in map(lambda val: val.lower(), lst):
-            raise ValueError("Invalid input. Data not in allowed values")
+            raise ValueError, "Invalid input. Data not in allowed values"
 
     # validate host network configuration
     # need  "ipaddress=,prefix=,gateway=,mac=,addr_type="
@@ -123,20 +155,20 @@ class HostSettingsObject(DbusProperties):
 
         # This has a hard data format mentioned above so no blanks allowed.
         if value.count(" ") or value.count("=") != 5:
-            raise ValueError("Invalid Network Data. No white spaces allowed")
+            raise ValueError, "Invalid Network Data. No white spaces allowed"
 
         config = value.split(',')
         for key_value in config:
-            key, value = key_value.split('=')
+            key , value = key_value.split('=')
             if not key or not value:
-                raise ValueError("Invalid key or Data")
+                raise ValueError, "Invalid key or Data"
 
             # Add the current key seen so we can compare at the end to see
             # if all values have been given
             user_config.append(key.lower())
 
             if key.lower() == 'ipaddress' or key.lower() == 'gateway':
-                IP(value)
+	        IP(value)
 
             elif key.lower() == 'mac':
                 regex = '([a-fA-F0-9]{2}[:|\-]?){6}'
@@ -151,8 +183,7 @@ class HostSettingsObject(DbusProperties):
 
         # Did user pass everything ??
         if set(all_config) - set(user_config):
-            raise ValueError(
-                "Invalid Network Data. All information is mandatory")
+            raise ValueError, "Invalid Network Data. All information is mandatory"
 
     # Validate to see if the changes are in order
     def input_validator(self, iface, proprty, value):
@@ -164,8 +195,7 @@ class HostSettingsObject(DbusProperties):
                 break
 
         # User entered key is not present
-        if not shk:
-            raise KeyError("Invalid Property")
+        if not shk: raise KeyError, "Invalid Property"
 
         if shk['validation'] == 'list':
             self.validate_list_ignore_case(shk['allowed'], value)
@@ -177,18 +207,22 @@ class HostSettingsObject(DbusProperties):
             self.validate_regex(shk['regex'], value)
 
         elif shk['validation'] == 'custom':
-            getattr(self, shk['method'])(value)
+	    getattr(self, shk['method'])(value)
 
 if __name__ == '__main__':
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
     bus = get_dbus()
-    obj = HostSettingsObject(bus, OBJ_NAME, s.SETTINGS, "/var/lib/obmc/")
-    mainloop = gobject.MainLoop()
+    allobjects = create_object(s.SETTINGS)
+    lastobject = ''
+    for oneset in allobjects:
+        if oneset[1] == lastobject:
+            continue
+        lastobject = oneset[1]
+        obj = HostSettingsObject(bus, oneset[1], allobjects, "/var/lib/obmc/")
+        mainloop = gobject.MainLoop()
 
-    obj.unmask_signals()
-    name = dbus.service.BusName(DBUS_NAME, bus)
+        obj.unmask_signals()
+        name = dbus.service.BusName(oneset[0], bus)
     print "Running HostSettingsService"
     mainloop.run()
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
