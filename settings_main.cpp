2c4db1bdf6db3795edaaf92fae6d34a698cba516
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-05-31 01:18:14 -0500

    Remove settings object root
    There is no need for a 'settings' root in the d-bus object namespace,
    because the settings objects are spread across existing namespaces
    appropriately.
    
    Change-Id: Ic012129b333cba8831c331803124b3e89642b536
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

--- settings_main.cpp
+++ settings_main.cpp
@@ -1,14 +1,10 @@
 #include <sdbusplus/bus.hpp>
-#include <sdbusplus/server/manager.hpp>
 #include "settings_manager.hpp"
 #include "config.h"
 
 int main(int argc, char *argv[])
 {
     auto bus = sdbusplus::bus::new_default();
-
-    // Add sdbusplus ObjectManager for the settings root.
-    sdbusplus::server::manager::manager objManager(bus, SETTINGS_ROOT);
 
     phosphor::settings::Manager mgr(bus);
 
